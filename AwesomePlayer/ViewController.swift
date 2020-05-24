087addd13e08a609638265f5559aaa2e52d2b036
Robert Crabtree <robert.crabtree@gmail.com>
2020-05-24 05:05:26 -0700

    Initial commit

---
+++ AwesomePlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomePlayer
+//
+//  Created by Rob Crabtree on 5/24/20.
+//  Copyright © 2020 Certified Organic Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
